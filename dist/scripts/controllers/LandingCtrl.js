a6970a39e807329265a9cf899c7c4520b574bb31
Jason <kovacic.jason@gmail.com>
2016-11-18 11:09:53 -0800

    Checkpoint #2

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -3,6 +3,20 @@
         // this.rooms = [{name: 'Blue'},{name: 'Red'},{name: 'Yellow'}]
 
         this.rooms = Room.all
+
+
+        window.foo = this.rooms
+
+
+        // this.rooms.$add({name: "Chat Room Name"})
+
+
+
+        this.clickFunction = function(){
+            console.log(1)
+        }
+
+
     }
 
     angular
